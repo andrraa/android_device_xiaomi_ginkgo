d8f49c5ec6927574f09d3391dd8fdbf45f2fcce4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-11-14 13:29:22 +0530

    ginkgo: power: Implement extension for LOW_POWER mode
     * for our kernelspace battery saver
     * inspired from https://github.com/LineageOS/android_device_xiaomi_sdm845-common/commit/8af17a83d41388c9fe3d4e550b702134293b40e4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -176,6 +176,9 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := trinket
 TRINKET := trinket
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
