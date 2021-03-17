a7c8254ab03d0557f83972cdeb32edbea62d6be8
LuK1337 <priv.luk@gmail.com>
2021-03-17 21:42:07 +0530

    ginkgo: Remove unnecessary languages_full.mk include
    * It's already included in full_base_telephony.
    
    Change-Id: Ia7bd27ce96e86eb38804caf670d034322d597ae3
    Signed-off-by: andrraa <andraramadanp@outlook.com>

--- nad_ginkgo.mk
+++ nad_ginkgo.mk
@@ -17,7 +17,6 @@
 # Inherit framework first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit from ginkgo device
