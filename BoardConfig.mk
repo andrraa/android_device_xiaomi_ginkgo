1b745468a89403402ea3d47b6952e3570d43bb94
andrraa <andraramadanp@outlook.com>
2021-05-28 00:10:47 +0000

    ginkgo: Uprev proton-clang version to 13.0
    Signed-off-by: andrraa <andraramadanp@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,11 +135,11 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/ginkgo
 TARGET_KERNEL_CONFIG := vendor/ginkgo-perf_defconfig
 
-KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-12.0/bin
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-13.0/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := 12.0
+TARGET_KERNEL_CLANG_VERSION := 13.0
 
 # Keystore
 TARGET_PROVIDES_KEYMASTER := true
